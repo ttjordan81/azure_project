0822878aa19b531734a31e391a062a0860c4e92a
Timothy Jordan <tjordan@ahpnet.com>
2023-02-21 23:28:34 -0500

    update url

--- app/infrastructure/authConfig.ts
+++ app/infrastructure/authConfig.ts
@@ -5,8 +5,8 @@ export const msalConfig = {
   auth: {
     clientId: 'd31b380a-b865-455e-864f-c2f47f15d95c',
     authority: 'https://login.microsoftonline.com/4c25b8a6-17f7-46f9-83f0-54734ab81fb1',
-    redirectUri: 'http://lively-meadow-0c02fb810.2.azurestaticapps.net', // Must be registered as a SPA redirectURI on your app registration
-    postLogoutRedirectUri: 'http://lively-meadow-0c02fb810.2.azurestaticapps.net' // Must be registered as a SPA redirectURI on your app registration
+    redirectUri: 'https://lively-meadow-0c02fb810.2.azurestaticapps.net/', // Must be registered as a SPA redirectURI on your app registration
+    postLogoutRedirectUri: 'https://lively-meadow-0c02fb810.2.azurestaticapps.net/' // Must be registered as a SPA redirectURI on your app registration
   },
   cache: {
     cacheLocation: 'localStorage'
